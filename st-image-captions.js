64269f6fa07d8b8c8553d172225a7e618477aeb7
shiftthis <shiftthis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-05-10 21:31:01 +0000

    git-svn-id: https://plugins.svn.wordpress.org/shiftthis-image-captions/trunk@12623 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ st-image-captions.js
@@ -0,0 +1,87 @@
+/* Javascript originally written by: Brett Chang of evercloud.com */
+/* Javascript altered to create one Div with a class of "caption" containing the image and the images alt-text displayed within a span for flexible styling abilities (plus it's a bit cleaner output and since an image is not tabular data, why would it be in a table?)*/
+
+var IMAGE_TRACKER = new Array();
+
+function addImgCaption(myImgElem)
+
+{
+
+ var myParent = myImgElem.parentNode;
+
+ var myParentParent = myImgElem.parentNode.parentNode;
+
+ var myWrapperTable = null;
+
+ var myNodeForAppend = myImgElem; function createDiv(myImgElem, myNodeForAppend)
+
+ {
+
+  var eDiv = document.createElement("div");
+
+  var eBr = document.createElement("br");
+  
+  var eSpan = document.createElement("span");
+
+  var eCaption =  document.createTextNode (myImgElem.getAttribute('alt'));
+
+//stylin'!
+
+  eDiv.className = "caption";
+  eDiv.style.width=myImgElem.getAttribute("width")+"px";
+  eDiv.style.cssFloat=myImgElem.getAttribute("align");
+  eDiv.style.styleFloat=myImgElem.getAttribute("align");
+	
+
+//create the table structure
+  eDiv.appendChild(myNodeForAppend);
+  eDiv.appendChild(eBr);
+  
+  var imgAlt = myNodeForAppend.getAttribute("alt");
+  var imgSrc = myNodeForAppend.getAttribute("src");  
+  var imgSplit = imgSrc.split("/");
+  var imgCount = (imgSplit.length-1);
+  var imgName = imgSplit[imgCount];
+
+  if(imgName != imgAlt && imgAlt != "" && imgAlt != null){
+	 	 eDiv.appendChild(eSpan);
+  		 eSpan.appendChild(eCaption);
+  }
+  
+  	myNodeForAppend.removeAttribute("alt");
+	myNodeForAppend.removeAttribute("title");
+    myNodeForAppend.removeAttribute("align");
+  
+return eDiv;
+
+ }
+
+ if(null == IMAGE_TRACKER[myImgElem.getAttribute('src')])
+
+ {
+
+if(myParent.nodeName == 'A'){
+
+   myNodeForAppend = myParent;
+
+   myWrapperDiv = createDiv(myImgElem, myNodeForAppend);
+
+   myParentParent.appendChild(myWrapperDiv);
+
+  }
+
+  else
+
+  {
+
+   myWrapperDiv = createDiv(myImgElem, myNodeForAppend);
+
+   myParent.appendChild(myWrapperDiv);
+
+  }
+
+  IMAGE_TRACKER[myImgElem.getAttribute('src')] = '1';
+
+ }
+
+}
